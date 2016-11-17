cebe0c3062ad8cf3f4708cf540b9d129e4b29684
CCDM20 <emir_gn13@hotmail.com>
2016-11-17 11:52:18 -0700

    mas brujeria

--- js/eventos.js
+++ js/eventos.js
@@ -5,8 +5,11 @@ var inicio =function(){
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
+    $("#imgFoto").show("slow");
+    $("#txtNombre").show("slow");
   	$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
   	$("#imgFoto").attr("src",data.results[0].picture.large);
+    $("#miArticle").html("texto");
     console.log(data.results[0].name.first+" "+data.results[0].name.last);
   }
 });
